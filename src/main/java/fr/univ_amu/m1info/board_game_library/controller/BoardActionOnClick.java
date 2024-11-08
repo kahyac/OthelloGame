322eb9cb48a5577bd6da2ca7602589dd209ef2dd
macbook <ahmed.kartout@etu.univ-amu.fr>
2024-11-08 15:39:30 +0100

    Revert to abort MVC

--- src/main/java/fr/univ_amu/m1info/board_game_library/controller/BoardActionOnClick.java
+++
@@ -1,5 +0,0 @@
-package fr.univ_amu.m1info.board_game_library.controller;
-
-public interface BoardActionOnClick {
-    void onClick(int row, int column);
-}
--- src/main/java/fr/univ_amu/m1info/board_game_library/view/BoardGameControllableView.java
+++
@@ -1,8 +0,0 @@
-package fr.univ_amu.m1info.board_game_library.view;
-
-import fr.univ_amu.m1info.board_game_library.controller.BoardGameController;
-
-
-public interface BoardGameControllableView extends BoardGameView {
-    void setController(BoardGameController controller);
-}
