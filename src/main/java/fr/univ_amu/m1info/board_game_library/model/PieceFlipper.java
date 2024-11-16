0c25ca6567f7e4be5ffbe4884593c81e3692843c
macbook <ahmed.kartout@etu.univ-amu.fr>
2024-11-16 20:58:18 +0100

    Update PieceFlipper

--- src/main/java/fr/univ_amu/m1info/board_game_library/model/PieceFlipper.java
+++ src/main/java/fr/univ_amu/m1info/board_game_library/model/PieceFlipper.java
@@ -1,12 +1,47 @@
 package fr.univ_amu.m1info.board_game_library.model;
 
 public class PieceFlipper {
-
     public void flipPieces(OthelloBoard board, int row, int col, Piece currentPlayer) {
-        // Flip pieces in all valid directions where captures are possible
+        int[][] directions = {
+                {-1, 0}, {1, 0}, {0, -1}, {0, 1},  // Up, down, left, right
+                {-1, -1}, {-1, 1}, {1, -1}, {1, 1} // Diagonals
+        };
+
+        for (int[] dir : directions) {
+            flipInDirection(board, row, col, dir[0], dir[1], currentPlayer);
+        }
     }
 
     private void flipInDirection(OthelloBoard board, int row, int col, int rowDir, int colDir, Piece currentPlayer) {
-        // Flip pieces along a specific direction
+        Piece opponent = (currentPlayer == Piece.BLACK) ? Piece.WHITE : Piece.BLACK;
+        int r = row + rowDir, c = col + colDir;
+        boolean hasOpponent = false;
+
+        // Check if there are any opponent pawns to turn over
+        while (r >= 0 && r < board.getSize() && c >= 0 && c < board.getSize()) {
+            if (board.getPieceAt(r, c) == opponent) {
+                hasOpponent = true;
+            } else if (board.getPieceAt(r, c) == currentPlayer) {
+                if (hasOpponent) {
+                    // Flip the pieces between (row, col) and (r, c)
+                    flipBetween(board, row, col, r, c, rowDir, colDir, currentPlayer);
+                }
+                break;
+            } else {
+                break;
+            }
+            r += rowDir;
+            c += colDir;
+        }
+    }
+
+    private void flipBetween(OthelloBoard board, int startRow, int startCol, int endRow, int endCol, int rowDir, int colDir, Piece currentPlayer) {
+        int r = startRow + rowDir, c = startCol + colDir;
+
+        while (r != endRow || c != endCol) {
+            board.placePiece(r, c, currentPlayer); // return the pawn
+            r += rowDir;
+            c += colDir;
+        }
     }
 }
